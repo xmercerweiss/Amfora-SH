ceb76fe3e943dbdaaef347adff017fb10b0dc7d2
Xavier Mercerweiss <mercerweissx@gmail.com>
2025-11-14 09:58:30 -0700

    Update amfora.sh

--- amfora.sh
+++ amfora.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-wget -O /usr/local/bin/amfora https://github.com/makew0rld/amfora/releases/download/v1.11.0/amfora_1.11.0_linux_64-bit
-chmod +x /usr/local/binamfora
-
+wget -O /usr/bin/amfora https://github.com/makew0rld/amfora/releases/download/v1.11.0/amfora_1.11.0_linux_64-bit
+chmod +x /usr/bin/amfora
+wget -O /usr/share/applications/amfora.desktop https://raw.githubusercontent.com/xmercerweiss/Amfora-SH/refs/heads/main/amfora.desktop
+wget -O /usr/share/icons/hicolor/512x512/apps/amfora.png https://github.com/xmercerweiss/Amfora-SH/blob/main/amfora_512x512.png?raw=true
